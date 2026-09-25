f69af04b81a09c4402ef35913076c0c07d00cc42
Guang Li <guang@lmd.ist.hokudai.ac.jp>
2026-09-25 22:36:17 +0900

    Remove outdated preprint release news

--- assets/data.js
+++ assets/data.js
@@ -13,8 +13,6 @@ const NEWS = [
   { date: "2026/07", html: 'Honored to be recognized as an Outstanding Reviewer for <a href="https://eccv.ecva.net/Conferences/2026" target="_blank" rel="noopener">ECCV 2026</a>!' },
   { date: "2026/06", html: 'Our paper has been accepted to <a href="https://eccv.ecva.net/Conferences/2026" target="_blank" rel="noopener">ECCV 2026</a>!' },
   { date: "2026/05", html: 'Honored to receive a Gold Reviewer Award at <a href="https://icml.cc/Conferences/2026" target="_blank" rel="noopener">ICML 2026</a>!' },
-  { date: "2026/05", html: '<a href="https://arxiv.org/abs/2605.07278" target="_blank" rel="noopener">RC-Aux for Latent World Models</a> was released!' },
-  { date: "2026/04", html: '<a href="https://arxiv.org/abs/2605.07194" target="_blank" rel="noopener">Closed-Form Linear-Probe Dataset Distillation</a> was released!' },
   { date: "2026/03", html: 'Invited to give a talk about DD at <a href="https://www.osaka-u.ac.jp/en" target="_blank" rel="noopener">The University of Osaka</a>!' },
   { date: "2026/03", html: 'Our research was featured in <a href="https://www.hokudai.ac.jp/news/2026/03/post-2211.html" target="_blank" rel="noopener">Hokkaido University Press</a>!' },
   { date: "2026/02", html: 'Invited to serve as an Area Chair for <a href="https://2026.acmmm.org/" target="_blank" rel="noopener">ACM MM 2026</a>!' },
